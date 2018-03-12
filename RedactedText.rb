79210756f43bc59b4d02088d3e30301bbabe51fe
Anna Burley <anna.burley3@gmail.com>
2018-03-12 21:17:11 +0000

    added redacted exersize

---
+++ RedactedText.rb
@@ -0,0 +1,15 @@
+puts "some text" 
+text = gets.chomp
+
+puts "word to redact"
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word == redact
+  print "REDACTED "
+    else 
+    print word + " "
+  end
+end
