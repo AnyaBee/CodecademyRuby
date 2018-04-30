ea0ec4848d9cef92a8a1d4adae85711c42767182
Anna Burley <anna.burley3@gmail.com>
2018-04-30 22:20:18 +0100

    Completed opposite number kata

---
+++ kata/opposite.rb
@@ -0,0 +1,10 @@
+#Very simple, given a number, find its opposite.
+#But can you do it in 1 line of code and without any conditionals?
+
+def opposite (x)
+ #your code here
+ x *= -1
+ puts x.to_s
+end
+
+opposite (-3)
